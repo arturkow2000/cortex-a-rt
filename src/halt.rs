1751fb947e2bcef1de2f7d27d2c0d178f8c8ed62
Artur Kowalski <arturkow2000@gmail.com>
2025-01-28 18:49:26 +0100

    Use wfi in __cortex_a_rt_platform_halt_default
    wfi puts CPU in sleep until interrupt occurs preventing CPU from
    spinning, needlessly consuming power and heating.

--- src/halt.rs
+++ src/halt.rs
@@ -1,4 +1,9 @@
 #[no_mangle]
 extern "C" fn __cortex_a_rt_platform_halt_default() -> ! {
-    loop {}
+    loop {
+        unsafe {
+            #[cfg(armv7a)]
+            core::arch::asm!("wfi", options(nomem, nostack));
+        };
+    }
 }
